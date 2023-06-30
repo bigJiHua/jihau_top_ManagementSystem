40a869cda81fdf314155849eb31d13f92da92e31
bigjihua <bigjihua@Gmail.com>
2023-06-30 11:54:40 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import RouterMap from '@/router/routerMap'
+import RouterMap from './routerMap'
 // layout 布局
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
