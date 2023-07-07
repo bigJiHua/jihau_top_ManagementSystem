81ed10bb93852e1d82040c79c1a7535d0d4cb1cf
bigjihua <bigjihua@Gmail.com>
2023-07-08 00:10:41 +0800

    Revert "2023年7月8日搞定富文本编辑器，但其余功能还未开始"
    This reverts commit 5a846086f15c859eed5c852d2294ddb08daae4f2.

--- src/views/ControlPanel/ArticleClass/ArticleEditor.vue
+++ src/views/ControlPanel/ArticleClass/ArticleEditor.vue
@@ -1,92 +1,39 @@
 <template>
-  <div class="EditorAreaBox">
-    <div class="EditorAside"></div>
-    <div id="EditorArea">
-      <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
-    </div>
+  <div>
+    <div id="editor"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
-import { Editor } from '@jihua/ckeditor5-jihua';
+import { onBeforeUnmount,onMounted } from 'vue';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-const editor = Editor
-const editorData = ref('<p>Content of the editor.</p>')
-const editorConfig = reactive({
-  plugins: Editor.builtinPlugins,
-  toolbar: {
-    items: [
-      'undo',
-      'redo',
-      'heading',
-      '|',
-      'bold',
-      'italic',
-      'link',
-      'bulletedList',
-      'numberedList',
-      '|',
-      'outdent',
-      'indent',
-      '|',
-      'codeBlock',
-      'showBlocks',
-      'selectAll',
-      'imageUpload',
-      'imageInsert',
-      'blockQuote',
-      'insertTable',
-      '-',
-      'highlight',
-      'horizontalLine',
-      'fontBackgroundColor',
-      'fontColor',
-      'alignment',
-      'fontFamily',
-      'fontSize',
-      'underline',
-      'superscript',
-      'strikethrough',
-      'specialCharacters',
-      'subscript',
-      'sourceEditing',
-      'removeFormat',
-      'htmlEmbed',
-    ],
-    shouldNotGroupWhenFull: true
-  },
-  language: 'zh-cn',
-  image: {
-    toolbar: [
-      'imageTextAlternative',
-      'toggleImageCaption',
-      'imageStyle:inline',
-      'imageStyle:block',
-      'imageStyle:side',
-      'linkImage'
-    ]
-  },
-  table: {
-    contentToolbar: [
-      'tableColumn',
-      'tableRow',
-      'mergeTableCells',
-      'tableCellProperties',
-      'tableProperties'
-    ]
-  }
-})
+let editorInstance : any;
+
+onBeforeUnmount(() => {
+  editorInstance.then((editor: { destroy: () => Promise<any>; }) => {
+    editor.destroy().catch((error: any) => {
+      console.error(error);
+    });
+  });
+});
+
+onMounted(() => {
+  ClassicEditor.create(document.querySelector('#editor') as HTMLElement, {
+    // 配置项
+  }).then(editor => {
+    editorInstance = Promise.resolve(editor);
+  }).catch(error => {
+    console.error(error);
+  });
+});
 </script>
-
-
 
 <style scoped>
 #editor {
-  height: calc(100vh - 70px);
+  min-height: 300px;
   border: 1px solid #ccc;
   border-radius: 4px;
   padding: 10px;
 }
 </style>
-
